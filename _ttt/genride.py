ff24b790c14211dc78d3d49b8c2a98c9126bce52
Jack Robbers <jack@2pisoftware.com>
2025-10-24 12:25:47 +1100

    add rides I remember / have data on

--- _ttt/genride.py
+++ _ttt/genride.py
@@ -1,3 +1,30 @@
-# generate a template md file for a ride
+# generates template md files for rides
+# rerun to add any missing weeks
 # easy since always on a thursday
 
+from datetime import timedelta, datetime
+from os import path
+
+template = '''---
+route: ???
+swim: ???
+---
+'''
+
+current_date = datetime(2023, 11, 2)
+week = timedelta(7)
+
+if __name__ == "__main__":
+    i = 1
+    while current_date < datetime.now():
+        filename = f'{datetime.strftime(current_date, "%Y-%m-%d")}.md'
+        if path.isfile(filename):
+            print(filename, i, 'already exists')
+        else:
+            with open(filename, 'w') as f:
+                f.write(template) 
+            print(f'created {filename} ride number {i}')
+        current_date 
+venue:= week
+        i 
+venue:= 1
--- ttt.md
+++ ttt.md
@@ -2,15 +2,31 @@
 layout: default
 ---
 
+<!-- TODO / IDEAS
+
+photos
+
+auto milestones. years, number of rides, daylight savings
+
+stats
+
+
+ -->
+
 Thick/Thicc Thigh Thursday is a weekly bike ride.
 
-We always meet at 6pm at Redfern Park
+We always meet at 6pm at Redfern Park.
+
+We've ridden {{ site.ttt | size }} times and swum at least {{  site.ttt | where:"swim","true" | size  }} times.
 
 # Ride Archive
 
-{% for ride in site.ttt %}
-{{ ride.date }} {{ ride.name }}
+{% for ride in site.ttt reversed %}
 
-{{ ride.collection }}
+{{ forloop.rindex }} **{{ ride.route }}{% if ride.venue %} + {% endif %}{{ ride.venue }}**   
+{{ ride.path | date: '%d/%m/%Y'}} 
 
+{{ ride.content }}
+
+---
 {% endfor %}
