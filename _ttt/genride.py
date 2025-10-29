5e5d1b6f69c9d637ae1b4a65fe774cdea10b36e8
Jack Robbers <jack@2pisoftware.com>
2025-10-29 13:24:15 +1100

    add rookwood ride

--- _ttt/genride.py
+++ _ttt/genride.py
@@ -9,7 +9,7 @@ week = timedelta(7)
 
 if __name__ == "__main__":
     i = 1
-    while current_date < datetime.now():
+    while current_date < datetime.now() + week:
         date = f'{datetime.strftime(current_date, "%Y-%m-%d")}'
         filename = f'{date}.md'
 
@@ -18,11 +18,10 @@ if __name__ == "__main__":
         else:
             with open(filename, 'w') as f:
                 template = f'''---
-                    route: ??? # (where did you go / what way)
-                    swim: ??? # (did you swim? yes/no)
-                    venue: # (where you ended up or ate)
-                    ---
-                '''
+route: ??? # (where did you go / what way)
+swim: ??? # (did you swim? yes/no)
+venue: # (where you ended up or ate)
+---'''
                 f.write(template) 
             print(f'created {filename} ride number {i}')
         current_date += week
