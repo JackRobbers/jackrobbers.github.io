b62e76c1d754542fde2e7ceb7104776c26e91b34
Jack Robbers <jack@2pisoftware.com>
2026-03-12 23:01:11 +1100

    break the site whoops

--- _ttt/genride.py
+++ _ttt/genride.py
@@ -4,25 +4,34 @@
 from datetime import timedelta, datetime
 from os import path
 
-current_date = datetime(2023, 11, 2, 18, 0, 0)
+start_date = datetime(2023, 11, 2, 18, 0, 0)
+current_date = start_date
 week = timedelta(7)
 
-if __name__ == "__main__":
-    i = 1
-    while current_date < datetime.now() + week:
-        date = f'{datetime.strftime(current_date, "%Y-%m-%d")}'
-        filename = f'{date}.md'
-
-        if path.isfile(filename):
-            print(filename, i, 'already exists')
-        else:
-            with open(filename, 'w') as f:
-                template = f'''---
+def make_ride(current_date):
+    date = f'{datetime.strftime(current_date, "%Y-%m-%d")}'
+    filename = f'{date}.md'
+    if path.isfile(filename):
+        print(filename, 'already exists')
+    else:
+        with open(filename, 'w') as f:
+            template = f'''---
 route: ??? # (where did you go / what way)
 swim: ??? # (did you swim? yes/no)
 venue: # (where you ended up or ate)
 ---'''
-                f.write(template) 
-            print(f'created {filename} ride number {i}')
+            f.write(template) 
+        print(f'created {filename}')
+
+if __name__ == "__main__":
+    i = 1
+    while current_date < datetime.now() + week:
+        make_ride(current_date)
         current_date += week
         i += 1
+
+    # add milestone rides
+    make_ride(start_date +  timedelta(weeks=i + 50 - i % 50))
+    # multiples of 50
+    # birthday
+    make_ride(start_date +  timedelta(weeks=52 * (datetime.now().year - start_date.year)))
--- ttt.md
+++ ttt.md
@@ -2,17 +2,6 @@
 layout: default
 ---
 
-<!-- TODO / IDEAS
-
-photos
-
-auto milestones. years, number of rides, daylight savings
-
-stats
-
-
- -->
-
 Thick/Thicc Thigh Thursday is a weekly bike ride.
 
 We always meet at 6pm at Redfern Park.
@@ -22,26 +11,29 @@ We've ridden {{ site.ttt | size }} times and swum at least {{  site.ttt | where:
 
 {% assign imageCount = 0 %}
 
+# Upcoming Rides (subject to change)
+
+{%  assign today = 'now' | date: '%s' %}
 
 # Ride Archive 
+{{ today }}
 
 <div class="flex-container" markdown=1>
 
 {% for ride in site.ttt reversed %}
+{% assign date_unix = ride.path | date: '%s'}
 
 <div markdown=1 id="{{date}}">
 
-
-{{ forloop.rindex }} **{{ ride.route }}{% if ride.venue %} + {% endif %}{{ ride.venue }}** {% if ride.swim == true %}🏊{% endif %} 
+{{ forloop.rindex }}
+**{{ ride.route }}{% if ride.venue %} + {% endif %}{{ ride.venue }}** {% if ride.swim == true %}🏊{% endif %} 
 
 {{ ride.path | date: '%d/%m/%Y' }}
-
 {% assign date = ride.path | split: "/" | last | split: "."  | first %}
 {% assign image_path = "/images/ttt/" | append: date %}
 {{ png_path}}
 {% assign images = site.static_files | where_exp: "file", "file.path contains image_path" | sort: "path" | reverse %}
 {% if images.size > 0 %}
-
 
 <div>
 {% for i in images  %}
