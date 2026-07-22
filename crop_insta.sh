f32bf1aef400d930a5bef50f0f7049a2f19dcc8a
Jack Robbers <jack@2pisoftware.com>
2026-07-22 21:15:11 +1000

    fix script and add ride

--- crop_insta.sh
+++ crop_insta.sh
@@ -2,7 +2,7 @@ thu=$(date +"%Y-%m-%d" -d "$(date +"%Y%m%d + 4 days - %w days")")
 
 height=$(magick identify -format "%H\n" $1)
 
-if [ $height -eq 1080 ]; then
+if [ $height -eq 2424 ]; then
         echo "height is correct, cropping"
         convert $1 -crop 1080x1920+0+223  images/ttt/$thu.png
         rm $1
